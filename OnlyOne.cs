41004b4bd2fdc88656e6455a6c5794ba052a6a74
Niko411 <36671168+niko411@users.noreply.github.com>
2018-02-20 19:20:05 +0300

    The second task

---
+++ OnlyOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = Convert.ToInt32(Console.ReadLine());
+            while (i != 1)
+            {
+                Console.WriteLine("Enter Again");
+                i = Convert.ToInt32(Console.ReadLine());
+
+            }
+                Console.WriteLine("Good Job");
+                Console.ReadKey();
+
+        }
+    }
+}
